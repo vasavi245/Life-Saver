fc815ba62f09e201e06c1adfbb229d5dfd7597bd
vasavi245 <r.vasavi04@gmail.com>
2020-08-05 14:55:38 +0800

    heroku

--- client/src/components/utils/API.js
+++ client/src/components/utils/API.js
@@ -6,12 +6,5 @@ export default {
         const token = localStorage.getItem("auth-token")
         return axios.get("/users/donors",{headers: {"x-auth-token": token}} );
     },
-
-    //delete user
-    deleteUser: function() {
-        const token = localStorage.getItem("auth-token");
-        return axios.delete("/users/delete",{headers: {"x-auth-token": token}} );
-
-    }
     
 }
